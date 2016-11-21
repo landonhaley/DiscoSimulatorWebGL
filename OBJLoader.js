ce87ca8db39227932d56027acc90d9e460a5427b
heyblackduck <r.median93@gmail.com>
2016-11-20 23:25:34 -0500

    Added some bare bones structure to loading an obj file and mtl file

---
+++ OBJLoader.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var OBJLoader = (function() {
+
+};
+
+//Reads in obj file contents, sets vertex, normals and faces
+OBJLoader.prototype.parseOBJ = function (fileContent) {
+
+}
+
+//Reads in MTL file contents, set materials, color, texture
+OBJLoader.prototype.parseMTL = function (fileContent) {
+
+}
+
+//Creates empty 32 bit buffer for WebGL
+OBJLoader.prototype.createBuffer = function () {
+
+}
+
+//Init WebGL buffers
+OBJLoader.prototype.initBuffers = function () {
+
+}
+
+//Reads valid obj file from a http request
+OBJLoader.prototype.readOBJFile = function (filePath) {
+
+}
+
+//Reads valid mtl file from a http request
+OBJLoader.prototype.readMTLFile = function (filePath) {
+
+}
+
+//Performs logic on obj file based on validity of file
+OBJLoader.prototype.onReadOBJFile = function () {
+
+}
+
+//Peforms logic on mtl file based on validity of file
+OBJLoader.prototype.onReadMTLFile = function () {
+
+}
