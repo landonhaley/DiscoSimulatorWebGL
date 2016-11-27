936433d9fb2697ef0a3c0ce6ed064c4f48d5b2c9
heyblackduck <r.median93@gmail.com>
2016-11-27 13:02:02 -0500

    added JSON files for the models, moved objloader to disco, disco now loads json files

--- disco.js
+++ disco.js
@@ -6,6 +6,44 @@
 
 var gl;
 
+function readTextFile(filePath) {
+    var fileText = new XMLHttpRequest();
+    fileText.open("GET", filePath, true);
+    fileText.onreadystatechange = function ()
+    {
+        if(fileText.readyState === 4)
+        {
+            if(fileText.status === 200 || fileText.status == 0)
+            {
+                alert(fileText.responseText);
+                loadJSON(JSON.parse(fileText.responseText));
+            }
+        }
+    }
+    fileText.send(null);
+}
+
+var vertexBuffer, normalBuffer, indexBuffer;
+
+function loadJSON(jsonData)
+{
+    normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(jsonData.vertexNormals), gl.STATIC_DRAW);
+    normalBuffer.itemSize = 3;
+    normalBuffer.numItems = jsonData.vertexNormals.length / 3;
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(jsonData.vertexPositions), gl.STATIC_DRAW);
+    vertexBuffer.itemSize = 3;
+    vertexBuffer.numItems = jsonData.vertexPositions.length / 3;
+    indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(jsonData.indices), gl.STATIC_DRAW);
+    indexBuffer.itemSize = 1;
+    indexBuffer.numItems = jsonData.indices.length;
+}
+
 function initGL(canvas)
 {
     try {
